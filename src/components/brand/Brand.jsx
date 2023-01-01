d80375f841da0e8bf1b79fd36e6344f044ad8751
André Marques <andrelcm89pt@gmail.com>
2023-01-01 18:02:28 +0000

    first commit

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './brand.css'
+import { atlassian, dropbox, google, shopify, slack } from './imports'
+
+//React functional component
+
+const Brand = () => {
+  return (
+    <div className='gpt3__brand section__padding'>
+        <div>
+          <img src={google} alt="google logo" />
+        </div>
+        <div>
+          <img src={slack} alt="slack logo" />  
+        </div>
+        <div>
+          <img src={atlassian} alt="atlassian logo" />
+        </div>
+        <div>
+          <img src={dropbox} alt="dropbox logo" />
+        </div>
+        <div>
+          <img src={shopify} alt="shopify logo" />  
+        </div>
+    </div>
+  )
+}
+
+export default Brand
